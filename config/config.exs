c107c6b4cbd1996157afaac1202e679472da4b8d
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2024-09-15 21:12:34 +0200

    add phoenix app

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,43 @@
 # move said applications out of the umbrella.
 import Config
 
+config :web,
+  ecto_repos: [Web.Repo],
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :web, Web.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [html: Web.ErrorHTML, json: Web.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: Web.PubSub,
+  live_view: [signing_salt: "nSwI7/Do"]
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  web: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../apps/web/assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.4.3",
+  web: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../apps/web/assets", __DIR__)
+  ]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +53,16 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+import Config
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
