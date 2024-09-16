5dd150c28c2cf66b6759f2f3ac0b656fac89bc03
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2024-09-16 19:45:51 +0200

    add login, register placeholder

--- apps/web/lib/web/controllers/page_controller.ex
+++ apps/web/lib/web/controllers/page_controller.ex
@@ -6,4 +6,12 @@ defmodule Web.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def login(conn, _params) do
+    render(conn, :login, layout: false)
+  end
+
+  def register(conn, _params) do
+    render(conn, :register, layout: false)
+  end
 end
--- apps/web/lib/web/router.ex
+++ apps/web/lib/web/router.ex
@@ -18,6 +18,8 @@ defmodule Web.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/login", PageController, :login
+    get "/register", PageController, :register
   end
 
   # Other scopes may use custom stacks.
