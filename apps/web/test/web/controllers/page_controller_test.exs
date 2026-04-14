ecb68b1bf087d9b7ab6aab7ba6edc806a49f5149
rafalsiniewicz <rafalsiniewicz@gmail.com>
2026-04-15 00:26:07 +0200

    fix CI errors

--- apps/web/test/web/controllers/page_controller_test.exs
+++ apps/web/test/web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 302) =~ "<html><body>You are being <a href=\"/login\">redirected</a>.</body></html>"
   end
 end
